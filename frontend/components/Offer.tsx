975c66084690cec2dac8f5b2fc1445b42b1b7e10
Adriantal <109103680+Adriantal1@users.noreply.github.com>
2023-02-16 09:46:57 +0100

    hmm

--- frontend/components/Offer.tsx
+++ frontend/components/Offer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image, { StaticImageData } from 'next/image'
 
-export default function Offer({image, text}:{image:StaticImageData, text:string}) {
+export default function Offer({ image, text }:{ image: StaticImageData, text: string }) {
   return (
     <div className='bg-white mt-20 p-5 max-w-sm flex justify-center mx-3 rounded-lg text-center shadow-md'>
       <ul>
