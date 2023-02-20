fda6ee0166ebc55dda80f91ae39d0b8f503eaa62
Adriantal <adik1237272@gmail.com>
2023-02-21 00:06:20 +0100

    Working carousel

--- frontend/components/Carousel.tsx
+++ frontend/components/Carousel.tsx
@@ -1,19 +1,50 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Offer from '@/components/Offer'
 import Egypt from '@/public/images/egipt.png'
 import France from '@/public/images/francja.png'
 import Bulgaria from '@/public/images/bulgaria.png'
-import { MdOutlineArrowForwardIos } from 'react-icons/md'
+import Malta from '@/public/images/malta.png'
+import Italy from '@/public/images/wlochy.png'
+import Cyprus from '@/public/images/cypr.png'
+import Czechia from '@/public/images/czechy.png'
+import Iceland from '@/public/images/islandia.png'
+import Poland from '@/public/images/polska.png'
+import { MdOutlineArrowForwardIos, MdOutlineArrowBackIosNew } from 'react-icons/md'
 
 export default function Carousel() {
+  const [slide, setSlide] = useState(0)
+
+  const pictures = [[Egypt,France,Bulgaria],[Malta,Italy,Cyprus],[Czechia,Iceland,Poland]]
+
+  // console.log(pictures.length)
+
+  function decrementSlide() {
+    if (slide>0) {
+      setSlide(prevSlide => prevSlide - 1)
+    }
+    else setSlide(pictures.length-1)
+  }
+
+  function incrementSlide() {
+    console.log(slide)
+    if (slide<pictures.length-1) {
+      console.log("kurwa?")
+      setSlide(prevSlide => prevSlide + 1)
+      console.log(slide)
+    }
+    else setSlide(0)
+  }
+
   return (
     <div>
-      <ul className='flex justify-center'>
-        <li><MdOutlineArrowForwardIos /></li>
-        <li><Offer image={Egypt} text="Egypt"/></li>
-        <li><Offer image={France} text="France"/></li>
-        <li><Offer image={Bulgaria} text="Bulgaria"/></li>
-        <li></li>
+      <ul className='flex justify-center items-center mt-20'>
+        <li><button><MdOutlineArrowBackIosNew onClick={decrementSlide} className='text-7xl text-white'/></button></li>
+        <li><Offer image={pictures[slide][0]} text="Egypt"/></li>
+        <li><Offer image={pictures[slide][1]} text="France"/></li>
+        <li><Offer image={pictures[slide][2]} text="Bulgaria"/></li>
+        <li><button><MdOutlineArrowForwardIos onClick={incrementSlide} className='text-7xl text-white'/></button></li>
       </ul>
     </div>
   )
--- frontend/components/Offer.tsx
+++ frontend/components/Offer.tsx
@@ -3,7 +3,7 @@ import Image, { StaticImageData } from 'next/image'
 
 export default function Offer({ image, text }:{ image: StaticImageData, text: string }) {
   return (
-    <div className='bg-white mt-20 p-5 max-w-sm flex justify-center mx-3 rounded-lg text-center shadow-md'>
+    <div className='bg-white p-5 max-w-sm flex justify-center mx-3 rounded-lg text-center shadow-md'>
       <ul>
       <li><Image src={image} alt=""/></li>
       <li>{text}</li>
