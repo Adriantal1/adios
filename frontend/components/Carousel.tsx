68eb814f5addf738a50d6452e67dcbba23372c8b
Adriantal <adik1237272@gmail.com>
2023-02-20 22:46:47 +0100

    icons

--- frontend/components/Carousel.tsx
+++ frontend/components/Carousel.tsx
@@ -3,14 +3,17 @@ import Offer from '@/components/Offer'
 import Egypt from '@/public/images/egipt.png'
 import France from '@/public/images/francja.png'
 import Bulgaria from '@/public/images/bulgaria.png'
+import { MdOutlineArrowForwardIos } from 'react-icons/md'
 
 export default function Carousel() {
   return (
     <div>
       <ul className='flex justify-center'>
-      <li><Offer image={Egypt} text="Egypt"/></li>
-      <li><Offer image={France} text="France"/></li>
-      <li><Offer image={Bulgaria} text="Bulgaria"/></li>
+        <li><MdOutlineArrowForwardIos /></li>
+        <li><Offer image={Egypt} text="Egypt"/></li>
+        <li><Offer image={France} text="France"/></li>
+        <li><Offer image={Bulgaria} text="Bulgaria"/></li>
+        <li></li>
       </ul>
     </div>
   )
