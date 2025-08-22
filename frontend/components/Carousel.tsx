b6b2eceae1b69cf9fd87c1a4f3ad7fd60e24b717
Adriantal <109103680+Adriantal1@users.noreply.github.com>
2025-08-22 11:45:31 +0000

    feat(carousel): shift visible items by one on navigation

--- frontend/components/Carousel.tsx
+++ frontend/components/Carousel.tsx
@@ -14,65 +14,54 @@ import Poland from '@/public/images/polska.png'
 import { MdOutlineArrowForwardIos, MdOutlineArrowBackIosNew } from 'react-icons/md'
 
 export default function Carousel() {
-  const [slide, setSlide] = useState(0)
+  // start is the index of the left-most visible item
+  const [start, setStart] = useState(0)
 
-  const pictures = [
-    [{ image: Egypt, text: 'Egypt', price: 3000, stars: 4}, { image: France, text: 'France', price: 2500, stars: 5 }, { image: Bulgaria, text: 'Bulgaria', price: 1501, stars: 4 }],
-    [{ image: Malta, text: 'Malta', price: 1329, stars: 4 }, { image: Italy, text: 'Italy', price: 1788, stars: 5 }, { image: Cyprus, text: 'Cyprus', price: 1699, stars: 3 }],
-    [{ image: Czechia, text: 'Czechia', price: 1058, stars: 4 }, { image: Iceland, text: 'Iceland', price: 8219, stars: 5 }, { image: Poland, text: 'Poland', price: 319, stars: 0 }],
+  const items = [
+    { image: Egypt, text: 'Egypt', price: 3000, stars: 4},
+    { image: France, text: 'France', price: 2500, stars: 5 },
+    { image: Bulgaria, text: 'Bulgaria', price: 1501, stars: 4 },
+    { image: Malta, text: 'Malta', price: 1329, stars: 4 },
+    { image: Italy, text: 'Italy', price: 1788, stars: 5 },
+    { image: Cyprus, text: 'Cyprus', price: 1699, stars: 3 },
+    { image: Czechia, text: 'Czechia', price: 1058, stars: 4 },
+    { image: Iceland, text: 'Iceland', price: 8219, stars: 5 },
+    { image: Poland, text: 'Poland', price: 319, stars: 0 },
   ]
 
+  const len = items.length
+
   function decrementSlide() {
-    if (slide>0) {
-      setSlide(prevSlide => prevSlide - 1)
-    }
-    else setSlide(pictures.length-1)
+    setStart(prev => (prev - 1 + len) % len)
   }
 
   function incrementSlide() {
-    console.log(slide)
-    if (slide<pictures.length-1) {
-      setSlide(prevSlide => prevSlide + 1)
-      console.log(slide)
-    }
-    else setSlide(0)
+    setStart(prev => (prev + 1) % len)
   }
 
   return (
     <div>
-      <ul className='flex justify-center items-center mt-20'>
+  <ul className='flex justify-center items-center mt-20' role="list" aria-label="Featured destinations">
         <li>
           <button aria-label="Previous slide" onClick={decrementSlide} className="p-2">
             <MdOutlineArrowBackIosNew className='text-7xl text-white'/>
           </button>
         </li>
-        <li>
-          <Offer 
-            image={pictures[slide][0].image} 
-            text={pictures[slide][0].text}  
-            price={pictures[slide][0].price!}
-            stars={pictures[slide][0].stars!}
-            alt={`${pictures[slide][0].text} photo`}
-          />
-        </li>
-        <li>
-          <Offer 
-            image={pictures[slide][1].image} 
-            text={pictures[slide][1].text} 
-            price={pictures[slide][1].price!}
-            stars={pictures[slide][1].stars!}
-            alt={`${pictures[slide][1].text} photo`}
-          />
-        </li>
-        <li>
-          <Offer 
-            image={pictures[slide][2].image} 
-            text={pictures[slide][2].text} 
-            price={pictures[slide][2].price}
-            stars={pictures[slide][2].stars}
-            alt={`${pictures[slide][2].text} photo`}
-          />
-        </li>
+        {[
+          items[start % len],
+          items[(start + 1) % len],
+          items[(start + 2) % len]
+        ].map((it, idx) => (
+          <li key={it.text} className="mx-1">
+            <Offer
+              image={it.image}
+              text={it.text}
+              price={it.price}
+              stars={it.stars}
+              alt={`${it.text} photo`}
+            />
+          </li>
+        ))}
         <li>
           <button aria-label="Next slide" onClick={incrementSlide} className="p-2">
             <MdOutlineArrowForwardIos className='text-7xl text-white'/>
